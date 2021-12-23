2ee137eb047b3ce36e2750b969af21d121fb6a31
kaolanet <1351207341@qq.com>
2021-12-23 16:31:54 +0800

    完善返回顶部

--- js/hao.js
+++ js/hao.js
@@ -438,6 +438,45 @@ function checkFunc(){
 	} catch(e) {}
 }
 
+
+// 返回顶部
+var obtn = document.getElementById('back-top');
+var timer2 = null;
+var isTop = true;
+//获取页面的可视窗口高度
+var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+//滚动条滚动时触发
+window.onscroll = function(){
+    //在滚动的时候增加判断
+    var osTop = document.documentElement.scrollTop || document.body.scrollTop;//特别注意这句，忘了的话很容易出错
+    if (osTop == 0) {
+        obtn.style.display = 'none';
+    }else{
+        obtn.style.display = 'block';
+    }
+    if (!isTop) {
+        clearInterval(timer2);
+    }
+    isTop = false;
+};
+obtn.onclick = function(){
+    //设置定时器
+    timer2 = setInterval(function(){
+        //获取滚动条距离顶部的高度
+        var osTop = document.documentElement.scrollTop || document.body.scrollTop;  //同时兼容了ie和Chrome浏览器
+        //减小的速度
+        var isSpeed = Math.floor(-osTop / 6);
+        document.documentElement.scrollTop = document.body.scrollTop = osTop + isSpeed;
+        //console.log( osTop + isSpeed);
+        isTop = true;
+        //判断，然后清除定时器
+        if (osTop == 0) {
+            clearInterval(timer2);
+        }
+    },30);  
+};
+
+
 (function($){
 	$(document).ready(function () {
 		timer = setInterval("checkFunc()","200");
